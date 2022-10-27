2f1e3309361fdbc186d3d985a7e78ba5e56beebd
ndakalajulius <julius.ndakala@student.moringaschool>
2022-10-27 06:56:54 +0300

    done

--- bin/run.rb
+++ bin/run.rb
@@ -1,1 +1,3 @@
 # require the environment file
+ruby
+require_relative '../config/environment'
--- config/environment.rb
+++ config/environment.rb
@@ -1,1 +1,3 @@
-# Set up Bundler here following the instructions in the README
+# Set up Bundler here following the instructions in the READMErequire 'bundler/setup'
+require 'bundler/setup'
+Bundler.require(:default, :development)
